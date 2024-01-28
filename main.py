e59442ff7b113501fdb10204ce35c35ed711be9a
Martyna Wilczak <martyna.wilczak97@gmail.com>
2024-01-28 12:45:00 +0100

    zadanie na 3

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import cv2
+
 
 app = Flask(__name__)
 api = Api(app)
+
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('images/pap_20230719_1DE.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(1, 1))
+
+        return {'counttt': len(boxes)}
 
 
 class HelloWorld(Resource):
@@ -10,6 +24,7 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
+api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
